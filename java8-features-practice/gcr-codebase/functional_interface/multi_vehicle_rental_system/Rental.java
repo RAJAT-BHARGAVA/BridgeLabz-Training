96d7d406b8dc4f5d4bcde1dce1f9091a291b355f
RAJAT-BHARGAVA <167312932+RAJAT-BHARGAVA@users.noreply.github.com>
2026-03-31 15:18:33 +0530

    Revert "Revert "Reviews""

--- java8-features-practice/gcr-codebase/functional_interface/multi_vehicle_rental_system/Rental.java
+++
@@ -1,6 +0,0 @@
-package functional_interface.multi_vehicle_rental_system;
-
-public interface Rental {
-	void rent(boolean availability);
-	void returnVehicle();
-}
---
+++ reviews/AddTwoMatrix.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class AddTwoMatrix{
+    public static void main(String [] args ){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [][]arr1 = new int[n][m];
+        int [][]arr2 = new int[n][m];
+        for(int i =0 ;i<n ;i++){
+            
+            for(int j =0 ;j<m ; j++){
+                arr1[i][j]=sc.nextInt();
+            }
+        }
+        for(int i =0 ;i<n ;i++){
+            for(int j =0 ;j<m ; j++){
+                arr2[i][j]=sc.nextInt();
+            }
+        }
+        int result [][] = new int[n][m];
+        // adding 
+        for (int i =0 ;i<n ;i++){
+            for(int j=0 ; j< m ;j++){
+                result[i][j] = arr1[i][j]+arr2[i][j];
+            }
+
+            
+        }
+        for(int i =0 ;i<n ;i++){
+            for(int j =0 ;j<m ; j++){
+                System.out.println(result[i][j]);
+            }
+        }
+    }
+}
+
