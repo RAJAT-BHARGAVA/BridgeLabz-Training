802a6078dc4bae6dada6be305da32b27360d32c2
Rajat Bhargava <rajatbhargav060404@gmail.com>
2026-07-31 17:23:33 +0530

    [Rajat] add. added Readme

--- core-java-practice/gcr-codebase/Programming-elements/Level01-Practice-Program/UniversityFee.java
+++
@@ -1,8 +0,0 @@
-public class UniversityFee {
-    public static void main(String[] args) {
-        int Fee = 125000; 
-        int discountPercentage = 10; 
-        double discountAmount = (Fee * discountPercentage) / 100.0;
-        System.out.println("The Discount Amount is INR " + discountAmount +" and final discounted fee is INR " + (Fee - discountAmount));
-    }
-}
